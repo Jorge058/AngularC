e96d74816c3b8281392e942aad3b551473d3696d
Jorge Martinez <102623373+Jorge058@users.noreply.github.com>
2024-09-12 15:24:30 -0600

    Contador terminado

--- Angular-Curso/02-bases/src/app/app.component.ts
+++ Angular-Curso/02-bases/src/app/app.component.ts
@@ -7,17 +7,5 @@ import { Component } from '@angular/core';
 
 /*El componente que nos sirve para trabajar el html */
 export class AppComponent {
-
   public title: string = 'HOLA MUNDO';
-  public counter: number = 10;
-
-  increaseBy(value: number):void{
-    this.counter += value;
-  }
-
-  resetCounter(): void{
-    this.counter = 10
-  }
-
-  
 }
--- Angular-Curso/02-bases/src/app/counter/counter.component.ts
+++ Angular-Curso/02-bases/src/app/counter/counter.component.ts
@@ -1,12 +1,30 @@
-import { Component, } from "@angular/core";
+import { Component } from "@angular/core";
 
 /* Hacemos que nuestra clase sea un componente con un decorador */
 @Component({
   selector: 'app-counter',
-  template: ``
-})
-export class CounterComponent{
+  template: `
 
+  <hr>
+  <h3> {{counter}} </h3>
+
+  <!-- Le agregamos la propiedad click a nuestro elemento
+  y ademas le asignamos el metodo para incrementar -->
+  <button (click)="increaseBy(+1)">+1</button>
+  <button (click)="resetCounter()">Reset</button>
+  <button (click)="increaseBy(-1)">-1</button>`
+})
+
+export class CounterComponent{
+  public counter: number = 10;
+
+  increaseBy(value: number):void{
+    this.counter += value;
+  }
+
+  resetCounter(): void{
+    this.counter = 10
+  }
 }
 
 
