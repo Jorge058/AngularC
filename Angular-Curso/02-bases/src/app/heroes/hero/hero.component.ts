7a7f4bd1b616c4c4800cd9fe8eba4b54e73f476a
Jorge Alberto Martinez Ramos-19120202 <jorge_halo@outlook.com>
2024-09-16 11:51:19 -0600

    ngIf

--- Angular-Curso/02-bases/src/app/heroes/hero/hero.component.ts
+++ Angular-Curso/02-bases/src/app/heroes/hero/hero.component.ts
@@ -11,5 +11,26 @@ export class HeroComponent {
   public name: string = 'ironman';
   public age: number = 45;
 
+  get capitalizedName(): string{
+    return this.name.toUpperCase();
+  }
 
+  getHeroDescription():string{
+    return `${this.name} - ${this.age}`;
+  }
+
+  //Seleccionamos el nombre y al hacer click al boton cambiamos el nombre nuevo
+  changeHero(): void{
+    this.name = 'Batman';
+  }
+
+  changeAge(): void{
+    this.age = 20;
+  }
+
+  resetForm(): void{
+    this.name='ironman'
+    this.age= 45
+  }
+  
 }
