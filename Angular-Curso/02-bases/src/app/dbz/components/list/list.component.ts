77d653932998d572d073f6f6208dacdf9dcb497a
Jorge Martinez <102623373+Jorge058@users.noreply.github.com>
2024-10-07 16:35:02 -0600

    Input

--- Angular-Curso/02-bases/src/app/dbz/components/list/list.component.ts
+++ Angular-Curso/02-bases/src/app/dbz/components/list/list.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'dbz-list',
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
-export class ListComponent {
 
+export class ListComponent {
+/* Cuando queremos exportar de un modulo padre a uno hijo
+debemos de indicar con un decorador @input() */
+  @Input()
+  public characterList: Character[] = [{
+    name: 'Trunks',
+    power: 10
+  }]
 }
