5822d842964ce651f303e3ed4510bf681c597042
Jorge Martinez <102623373+Jorge058@users.noreply.github.com>
2024-10-25 15:36:06 -0600

    uuid
    Instalar uuid y terminar el ejercicio

--- Angular-Curso/02-bases/src/app/dbz/services/dbz.service.ts
+++ Angular-Curso/02-bases/src/app/dbz/services/dbz.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
+/*
+  ? Instalamos uuid que es una libreria para asignar indices a los elementos.
+  * Instalamos como npm i uuid.
+  ? Como es un archivo escrito en vanilla debemos instalar un entorno de desarrollo
+  * Instalamos con npm i --save-dev @types/uuid
+    */
+import { v4 as uuid} from 'uuid';
 import { Character } from '../interfaces/character.interface';
 
 /*
-   ?Injectable es un decorador que le dice a angular que es un servicio
+  ?Injectable es un decorador que le dice a angular que es un servicio
   ? y que se puede injectar
 */
 @Injectable({
@@ -11,24 +18,34 @@ import { Character } from '../interfaces/character.interface';
 export class DbzService {
 
   public characters: Character[] = [{
+    id: uuid(),
     name:' Krilin',
     power: 1000
   },{
+    id: uuid(),
     name:'Goku',
     power: 9500
   },{
+    id: uuid(),
     name: 'Vegeta',
     power: 7500
   }];
 
-  /* Creamos la funcion donde vamos a recibir el objeto
-  de padre a hijo */
+  /*
+  ? Creamos la funcion donde vamos a recibir el objeto
+  ? de padre a hijo
+  */
 
   onNewCharacter(character:Character):void{
-    this.characters.push(character);
+    /*
+    ? Utilizamos el spreed para que se genere un id en los nuevos objetos
+    */
+    const newCharacter: Character = {id: uuid(), ...character};
+    this.characters.push(newCharacter);
   }
 
-  onDeleteCharacter(index:number){
-    this.characters.splice(index, 1)
+  deleteCharacterById(id:string){
+    this.characters = this.characters.filter(character => character.id !== id);
   }
+
 }
