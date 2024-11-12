1d8bd359b7dc1b7ab4cd69bb4082c4245cbaf53b
Jorge Martinez <102623373+Jorge058@users.noreply.github.com>
2024-11-12 10:42:41 -0600

    Gifs terminados
    Sesion 3 terminada

--- Angular-Curso/03-gift-app/src/app/gifs/services/gifs.service.ts
+++ Angular-Curso/03-gift-app/src/app/gifs/services/gifs.service.ts
@@ -16,7 +16,10 @@ export class GifsService {
   private apiKey = 'pACkLqaDHy0gPYQL6RHyGfxjgX1KnoDV';
   private serviceUrl = 'https://api.giphy.com/v1/gifs'
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.loadLocalStorage();
+    console.log('Gifs');
+  }
 
   get tagsHistory(){
     return [...this._tagsHistory];
@@ -43,8 +46,18 @@ export class GifsService {
     localStorage.setItem('history',JSON.stringify(this._tagsHistory))
   }
 
-  private loadLocalStorage(){
-    const temporal = 
+  /*
+  *Creamos la funcion en donde vamos a llamar al localstorage para verificar si ya se guardo algo
+  *anteriormente
+ */
+  private loadLocalStorage():void{
+    if (!localStorage.getItem('history')) return;
+
+    this._tagsHistory = JSON.parse(localStorage.getItem('history')!);
+
+    //*Regresamos el valor guardado como ultimo, de esta manera al recargar se mostrara la ultima busqueda
+    if(this._tagsHistory.length === 0) return;
+    this.searchTag( this._tagsHistory[0]);
   }
 
   searchTag(tag:string):void{
