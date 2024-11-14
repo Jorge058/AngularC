805e0dd57e52610b6c38c369855886d4a9a0da31
Jorge Alberto Martinez Ramos-19120202 <jorge_halo@outlook.com>
2024-11-13 21:35:17 -0600

    Sesion casi terminada

--- Angular-Curso/03.5-gift-app-plus/src/app/gifs/gifs.module.ts
+++ Angular-Curso/03.5-gift-app-plus/src/app/gifs/gifs.module.ts
@@ -3,7 +3,8 @@ import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home/home-page.component';
 import { SearchBoxComponent } from './Components/search-box/search-box.component';
 import { CardListComponent } from './Components/card-list/card-list.component';
-import { CardComponent } from './components/card/card.component';
+import { CardComponent } from './Components/card/card.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 
@@ -16,7 +17,7 @@ import { CardComponent } from './components/card/card.component';
   ],
   imports: [
     CommonModule,
-
+    SharedModule
   ],
   exports:[
     HomePageComponent
