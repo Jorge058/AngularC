48a1bcbec5c938a4a1e51959f8151ed9d633979b
Jorge Martinez <102623373+Jorge058@users.noreply.github.com>
2025-01-23 11:15:19 -0600

    Debouncer

--- Angular-Curso/04-countryApp/src/app/shared/components/search-box/search-box.component.ts
+++ Angular-Curso/04-countryApp/src/app/shared/components/search-box/search-box.component.ts
@@ -1,11 +1,15 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { debounceTime, Subject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './search-box.component.html',
   styles: ``
 })
-export class SearchBoxComponent {
+export class SearchBoxComponent implements OnInit, OnDestroy {
+
+  private debouncer: Subject<string> = new Subject<string>
+  private debouncerSuscription?: Subscription;
 
   @Input()
   public placeholder:string = '';
@@ -13,7 +17,34 @@ export class SearchBoxComponent {
   @Output()
   public onValue = new EventEmitter<string>();
 
+  @Output()
+  public onDebounce = new EventEmitter<string>();
+
+  /* Se encarga de hacer las emisiones del onkeypress */
+  ngOnInit():void {
+    this.debouncerSuscription = this.debouncer
+    .pipe(
+      debounceTime(1000)
+    )
+    .subscribe(value => {
+      this.onDebounce.emit(value);
+    })
+  }
+
+  /*
+    Se manda a llamar cuando la instancia del componente se destruye,
+    siempre que usamos una suscripcion debemos terminarla, ya que de no hacerlo
+    se estará escuchando indefinidamente
+  */
+  ngOnDestroy(): void {
+    this.debouncerSuscription?.unsubscribe();
+  }
+
   emitValue( value:string ):void{
     this.onValue.emit(value);
+  }
+
+  onKeyPress(searchTerm: string){
+    this.debouncer.next(searchTerm);
   }
 }
