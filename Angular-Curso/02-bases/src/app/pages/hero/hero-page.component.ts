f2b6d593f80056779d0f4ae4e3f4ad8627b073f3
Jorge Martinez <102623373+Jorge058@users.noreply.github.com>
2025-05-26 17:00:34 -0600

    RouterLink

--- Angular-Curso/02-bases/src/app/pages/hero/hero-page.component.ts
+++ Angular-Curso/02-bases/src/app/pages/hero/hero-page.component.ts
@@ -1,13 +1,25 @@
-import { Component, signal } from "@angular/core";
+import { UpperCasePipe } from "@angular/common";
+import { Component, computed, signal } from "@angular/core";
 
 @Component({
   templateUrl: './hero-page.component.html',
-
+  imports: [UpperCasePipe],
 })
 
 export class HeroPageComponent{
   name = signal('IronMan');
   age = signal(45);
+
+  //*Una señal computada es una señal que solo se lee
+  heroDescription = computed(() => {
+    const description = `${this.name()}-${this.age()}`;
+    return description;
+  });
+
+  capitalizedName = computed(()=>{
+    const capitalized = `${this.name().toLocaleUpperCase()}`;
+    return capitalized;
+  })
 
   getHeroDescription(){
     return `${this.name()} - ${this.age()}`
