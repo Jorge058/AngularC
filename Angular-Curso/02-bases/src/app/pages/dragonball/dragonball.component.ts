4fb008036ee4eeb17363bdf571ea16007a2747be
Jorge Martinez <102623373+Jorge058@users.noreply.github.com>
2025-05-29 16:30:27 -0600

    Agregar character

--- Angular-Curso/02-bases/src/app/pages/dragonball/dragonball.component.ts
+++ Angular-Curso/02-bases/src/app/pages/dragonball/dragonball.component.ts
@@ -1,4 +1,5 @@
-import { Component, signal } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, computed, signal } from '@angular/core';
 
 interface Character{
   id:number,
@@ -7,15 +8,52 @@ interface Character{
 }
 
 @Component({
-  imports: [],
+  imports: [
+    //NgClass
+  ],
   templateUrl: './dragonball-page.component.html'
 })
 
 
 export class DragonballPageComponent {
+  name = signal('Gohan');
+  power = signal(100);
+
   characters = signal<Character[]>([
-  {id: 1, name:'Goku', power:9001},
-  {id: 2, name:'Vegeta', power:8000},
-  {id: 3, name:'Piccolo', power:7000}
+    {id: 1, name:'Goku', power:9001},
+    {id: 2, name:'Vegeta', power:9000},
+    {id: 3, name:'Piccolo', power:7000},
+    {id: 4, name:'Yamcha', power:500}
   ]);
-}
+
+  addCharacter(){
+    if (!this.name() || !this.power() || +this.power() <= 0) {
+      return;
+    }
+
+    const newCharacter: Character = {
+      id: this.characters().length + 1,
+      name: this.name(),
+      power: +this.power(),
+    };
+
+    /*
+    ?Como no es recomendable trabajar con los metodos para las señales
+    ?utilizamos el update para actualizar nuestra señal */
+    this.characters.update((list) => [...list, newCharacter]);
+    this.resetFields();
+  }
+
+  resetFields() {
+    this.name.set('');
+    this.power.set(0);
+  }
+
+  /* powerClasses = computed(()=> {
+    return {
+      'text-danger': true,
+      }
+  }) */
+
+  }
+
