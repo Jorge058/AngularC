497c03616e99c03f485fe8550a3ecb9275dbd043
Jorge Alberto Martinez Ramos-19120202 <jorge_halo@outlook.com>
2025-06-16 21:25:17 -0600

    Country-function

--- Angular-Curso/04-country-app/src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ Angular-Curso/04-country-app/src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { SearchInputComponent } from "../../components/search-input/search-input.component";
+import { CountryListComponent } from "../../components/country-list/country-list.component";
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-by-capital-page',
-  imports: [],
+  imports: [SearchInputComponent, CountryListComponent],
   templateUrl: './by-capital-page.component.html',
 })
 export class ByCapitalPageComponent {
 
-  onSearch(value:string){
-    console.log(value);
+  countryService = inject(CountryService);
 
+  onSearch(query:string){
+    this.countryService.searchByCapital(query)
+    .subscribe(resp => {
+      console.log(resp);
+    })
   }
 }
