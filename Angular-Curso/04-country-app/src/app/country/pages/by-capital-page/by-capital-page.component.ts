6a4737600a8bd63fbef6a4ec3e4ce4294ae77421
Jorge Martinez <102623373+Jorge058@users.noreply.github.com>
2025-06-19 16:42:44 -0600

    Continuar con resource

--- Angular-Curso/04-country-app/src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ Angular-Curso/04-country-app/src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,8 +1,9 @@
-import { Component, inject, signal } from '@angular/core';
-import { SearchInputComponent } from "../../components/search-input/search-input.component";
-import { CountryListComponent } from "../../components/country-list/country-list.component";
+import { Component, inject, resource, signal } from '@angular/core';
+import { SearchInputComponent } from '../../components/search-input/search-input.component';
+import { CountryListComponent } from '../../components/country-list/country-list.component';
 import { CountryService } from '../../services/country.service';
 import { Country } from '../../interfaces/country.interface';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -10,25 +11,48 @@ import { Country } from '../../interfaces/country.interface';
   templateUrl: './by-capital-page.component.html',
 })
 export class ByCapitalPageComponent {
-
   countryService = inject(CountryService);
+  query = signal('');
+/*
+? Utilizamos un Resource para obtener una respuesta y un loader, el cual
+?se va a encargar de hacer la peticion al servicio y evitamos hacer codigo
+?tradicional
+ */
+  countryResource = resource({
+    request: () => ({query: this.query()}),
+    loader: async({request}) => {
+      if (!this.query()) return []
+      
+      /* First value permite transformar cualquier obserbable en promesas */
+      return await firstValueFrom(
+        this.countryService.searchByCapital(request.query)
+      )
+    }
+  })
+
+  /* Forma traicional para manejar la respuesta
 
   isLoading = signal(false);
-  isError = signal<string|null>(null);
+  isError = signal<string | null>(null);
   countries = signal<Country[]>([]);
 
-  onSearch(query:string){
-    if(this.isLoading()) return;
+  onSearch(query: string) {
+    if (this.isLoading()) return;
 
     this.isLoading.set(true);
     this.isError.set(null);
 
-    this.countryService.searchByCapital(query)
-    .subscribe((countries) => {
-      this.isLoading.set(false);
-      this.countries.set(countries);
-
-      console.log(countries);
-    })
-  }
+    this.countryService.searchByCapital(query).subscribe({
+      next: (countries) => {
+        this.isLoading.set(false);
+        this.countries.set(countries);
+      },
+      error: (err) => {
+        this.isLoading.set(false);
+        this.countries.set([]);
+        this.isError.set(err)
+      },
+    },
+  );
+  } */
 }
