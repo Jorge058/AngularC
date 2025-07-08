7ea0aaecb365994e76243c05ccc8720e8af2e4b8
Jorge Alberto Martinez Ramos-19120202 <jorge_halo@outlook.com>
2025-07-07 21:37:22 -0600

    Seccion 4 terminada

--- Angular-Curso/04-country-app/src/app/country/services/country.service.ts
+++ Angular-Curso/04-country-app/src/app/country/services/country.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 
 import { RESTCountry } from '../interfaces/res-countries.interfaces';
-import { catchError, map, Observable, throwError } from 'rxjs';
+import { catchError, delay, map, Observable, throwError } from 'rxjs';
 import type { Country } from '../interfaces/country.interface';
 import { CountryMapper } from '../mappers/country.mapper';
 
@@ -35,6 +35,7 @@ export class CountryService {
 
     return this.http.get<RESTCountry[]>(url).pipe(
       map((resp) => CountryMapper.mapRestCountryArrayToCountryArray(resp)),
+      delay(3000),
       catchError((error) => {
         console.log('Error fetching', error);
 
@@ -44,5 +45,21 @@ export class CountryService {
       })
     );
   }
-  
+
+  searchCountryByAlphaCode(code: string) {
+    const url = `${API_URL}/name/${code}`;
+
+    return this.http.get<RESTCountry[]>(url).pipe(
+      map((resp) => CountryMapper.mapRestCountryArrayToCountryArray(resp)),
+      map(countries => countries.at(0)),
+      catchError((error) => {
+        console.log('Error fetching', error);
+
+        return throwError(
+          () => new Error(`No se pudo obtener la busqueda con ese codigo: ${code}`)
+        );
+      })
+    );
+  }
+
 }
