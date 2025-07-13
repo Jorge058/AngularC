db3aeb248342a5106300cb2559b0d853dbe08724
Jorge Alberto Martinez Ramos-19120202 <jorge_halo@outlook.com>
2025-07-13 13:50:22 -0600

    Pipes
    Pipes basicos y numericos.
    Cambio de idioma

--- Angular-Curso/05-pipes-app/src/app/app.config.ts
+++ Angular-Curso/05-pipes-app/src/app/app.config.ts
@@ -1,8 +1,37 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { registerLocaleData } from '@angular/common';
 
 import { routes } from './app.routes';
 
+/*
+?Para hacer que nuestra app cambie de idioma debemos de hacer varias cosas.
+* 1- Aqui importamos el idioma  que vamos a utilizar*/
+
+import localEs from '@angular/common/locales/es-MX'
+import localPl from '@angular/common/locales/pl'
+import { LocaleService } from './services/locale.service';
+
+/*
+*2- Resgistramos el local en la variable registerLocalData
+ */
+registerLocaleData(localEs,'es');
+registerLocaleData(localPl,'pl')
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+
+    /*
+    ?Para hacerlo dinamicamente debemos importar el servicio como una
+    ?depenencia, luego debemos utilizar el useFactory que se utiliza para disparar
+    ?el servicio cuando el provedor se inicializa*/
+    {
+      provide: LOCALE_ID,
+      //useValue: 'pl'
+      deps:[LocaleService],
+      useFactory:(localeService: LocaleService) => localeService.getLocale,
+    }
+  ]
 };
---
+++ Angular-Curso/05-pipes-app/src/app/pages/basic-page/basic-page.component.ts
@@ -0,0 +1,38 @@
+import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { Component, effect, inject, LOCALE_ID, signal } from '@angular/core';
+import { AvailableLocale, LocaleService } from '../../services/locale.service';
+
+@Component({
+  selector: 'app-basic-page',
+  imports: [LowerCasePipe,UpperCasePipe,TitleCasePipe,DatePipe],
+  templateUrl: './basic-page.component.html',
+})
+
+export default class BasicPageComponent {
+  localeService =inject(LocaleService);
+  currentLocale = signal(inject(LOCALE_ID));
+
+  nameLower = signal('Jorge');
+  nameUpper = signal('JORGE');
+  fullName = signal('JOrgE AlbErtO');
+
+  customDate = signal(new Date());
+
+  tickingDateEffect = effect((onCleanup)=>{
+    const interval = setInterval(()=>{
+      this.customDate.set(new Date())
+    },1000);
+
+    /*
+    !Cuando hacemos un efecto debemos de limpiarlo, ya que si navegamos a otra
+    !pagina, se va a crear otro efecto. Es mejor limpiarlo con un clearInteral()
+    */
+    onCleanup(()=>{
+      clearInterval(interval)
+    })
+  })
+
+  changeLocale(locale: AvailableLocale){
+    this.localeService.changeLocale(locale)
+  }
+}
